bf84bdd6382dc3e055530e76ac89b651f7940586
DudulClaudiul <dudulhimslef@gmail.com>
2026-05-19 20:55:02 +0300

    added if statements and odd even calculations

--- 04_git.py
+++ 04_git.py
@@ -4,3 +4,25 @@ print("Hello World")
 
 print("this is a change")
 print("this is another one")
+
+#if-uri
+
+nr_pare = []
+nr_impare = []
+
+lista2 = [6, 7, 10, 90, 100, 33, 88, 5, 13, 0]
+for nr in lista2:
+    if nr % 2 == 0:
+        nr_pare.append(nr)
+    else:
+        nr_impare.append(nr)
+
+print("Nr. pare: ", nr_pare)
+print("Nr. impare: ", nr_impare)
+
+# expresii logice
+
+for nr in lista2:
+    if nr % 2 == 0 and nr % 5 == 0:
+        print("Nr urmator e si par, si multiplu de 5: ")
+        print(nr)
